14b65d1ca314ad096c34d14d40ba227f951b45f2
sbera7 <iamshreyangshu07@gmail.com>
2025-02-15 12:53:52 -0800

    initial commit

---
+++ demo.sh
@@ -0,0 +1,14 @@
+# rebuild pass
+export LLVMDIR=export LLVM_DIR=/usr/lib/llvm-19
+cd build
+cmake -DLT_LLVM_INSTALL_DIR=$LLVM_DIR ..
+make
+
+# move to example file
+cd ..
+
+# convert input.c to input.bc
+clang-19 -c -emit-llvm -fno-discard-value-names -O0 demo.c -o demo.bc
+
+# run with the pass
+opt-19 -load-pass-plugin build/lib/libHelloWorld.so -passes=hello-world -disable-output demo.bc
